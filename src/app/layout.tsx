0556e448e03e80cb881273b76c39c402d5cf3f5a
David Chu <davidchuyaya@gmail.com>
2026-01-06 22:05:04 -0800

    Better favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,10 @@ export default function RootLayout({
         <html lang="en">
             <head>
                 <link rel="stylesheet" href="https://use.typekit.net/gpm8jeo.css"></link>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+                <link rel="manifest" href="/site.webmanifest"></link>
             </head>
             <body className="antialiased">
                 <CursorGlow />
