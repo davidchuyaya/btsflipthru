5f87c9e82922675b4b0a90e6a003ca3b0853762a
David Chu <davidchuyaya@gmail.com>
2026-01-10 23:08:16 -0800

    Report fixes

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,28 +1,11 @@
-"use client";
+import { Metadata } from "next";
+import ReportClientPage from "./report-client-page";
 
-import { ReportType } from "@/constants";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
-import ReportForm from "./report";
-
-export const metadata = {
+export const metadata: Metadata = {
     title: "Report Feedback | BTS Flipthru",
     description: "Let us know if something's wrong with BTS Flipthru.",
 };
 
-function ReportPageSuspenseWrapper() {
-    const params = useSearchParams();
-    const reportType = params.get("reportType") as ReportType;
-    const title = params.get("title");
-    const url = params.get("url");
-
-    return <ReportForm reportType={reportType} title={title} url={url} />;
-}
-
-export default function ReportPageComponent() {
-    return (
-        <Suspense fallback={<div className="flex justify-center m-8">Loading...</div>}>
-            <ReportPageSuspenseWrapper />
-        </Suspense>
-    );
+export default function ReportPage() {
+    return <ReportClientPage />;
 }
---
+++ src/app/report/report-client-page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ReportType } from "@/constants";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import ReportForm from "./report";
+
+function ReportPageSuspenseWrapper() {
+    const params = useSearchParams();
+    const reportType = params.get("reportType") as ReportType;
+    const title = params.get("title");
+    const url = params.get("url");
+
+    return <ReportForm reportType={reportType} title={title} url={url} />;
+}
+
+export default function ReportClientPage() {
+    return (
+        <Suspense fallback={<div className="flex justify-center m-8">Loading...</div>}>
+            <ReportPageSuspenseWrapper />
+        </Suspense>
+    );
+}
