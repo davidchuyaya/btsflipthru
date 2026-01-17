dba477201c42351bff8c15e51c2c54639aed35c8
David Chu <davidchuyaya@gmail.com>
2026-01-16 19:26:44 -0800

    Trying to get google rich results

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
                             description,
                             url,
                             image,
-                        }),
+                        }).replace(/</g, '\\u003c'),
                     }}
                 />
             </head>
